daa78b27e91ac3fba1ab14e2f5b6fc738d8ec6b6
Vishva Maithri <67517037+vishva-maithri@users.noreply.github.com>
2026-09-18 14:03:46 +1000

    Route YouTube player window

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import YouTubePopup from './YouTubePopup'
 import './styles.css'
-createRoot(document.getElementById('root')!).render(<StrictMode><App/></StrictMode>)
+
+const isYoutubePopup=window.location.pathname==='/youtube-player'
+const Root=isYoutubePopup?YouTubePopup:App
+
+createRoot(document.getElementById('root')!).render(<StrictMode><Root/></StrictMode>)
